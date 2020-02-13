7da51235c4c0f4535e822d70d0546bb66888d784
Pauline2897 <57684631+Pauline2897@users.noreply.github.com>
2020-02-13 16:15:16 +0100

    Update PYTHON_fracs.py

--- PYTHON_fracs.py
+++ PYTHON_fracs.py
@@ -1,106 +1,88 @@
-from math import gcd
+import fractions
 
-class Frac:
-    def __init__(self, numerator, *denominator):
-        try:
-            assert denominator[0] != 0
-            self.numerator = int(numerator)
-            self.denominator = int(denominator[0])
-            g = gcd(self.denominator, self.numerator)
-            if g > 1:
-                self.denominator /= g
-                self.numerator /= g
-        except AssertionError:
-            raise ValueError
-        except IndexError:
-            self.numerator = int(numerator)
-            self.denominator = 1
+def reduce_frac(frac):
+    div = fractions.gcd(frac[0], frac[1])
+    frac[0] /= div
+    frac[1] /= div
+    return frac
 
-    def __add__(self, other):
-        return Frac(self.numerator * other.denominator + self.denominator * other.numerator,
-                    self.denominator * other.denominator)
+def add_frac(frac1, frac2):
+    numerator = frac1[0]*frac2[1] + frac2[0]*frac1[1]
+    denominator = frac1[1]*frac2[1]
+    return reduce_frac([numerator,denominator])
 
-    def __sub__(self, other):
-        return Frac(self.numerator * other.denominator - self.denominator * other.numerator,
-                    self.denominator * other.denominator)
-
-    def __mul__(self, other):
-        return Frac(self.numerator * other.numerator,
-                    self.denominator * other.denominator)
-
-    def __truediv__(self, other):
-        return Frac(self.numerator * other.denominator,
-                    self.denominator * other.numerator)
-
-    def __lt__(self, other):
-        first = self.numerator * other.denominator
-        second = self.denominator * other.numerator
-        return first < second
-
-    def is_positive(self):
-        return False if self.numerator * self.denominator < 0 else True
-
-    def is_zero(self):
-        return False if self.numerator else True
-
-    def __float__(self):
-        return self.numerator / self.denominator
-
-    def __repr__(self):
-        return "{0} / {1}".format(self.numerator, self.denominator)
-
-    def __eq__(self, other):
-        if self.denominator == other.denominator and self.numerator == other.numerator:
-            return True
-        elif self.numerator == self.denominator and other.numerator == other.denominator:
-            return True
-        else:
-            return False
+def sub_frac(frac1, frac2):
+    numerator = frac1[0]*frac2[1]-frac2[0]*frac1[1]
+    denominator = frac1[1]*frac2[1]
+    return reduce_frac([numerator,denominator])
 
 
+def mul_frac(frac1, frac2):
+    numerator = frac1[0]*frac2[0]
+    denominator = frac1[1]*frac2[1]
+    return reduce_frac([numerator,denominator])
 
+def div_frac(frac1, frac2):
+    numerator = frac1[0] * frac2[1]
+    denominator = frac1[1] * frac2[0]
+    return reduce_frac([numerator, denominator])
 
+def is_positive(frac):
+    return frac[0]/float(frac[1])>0
+
+def is_zero(frac):
+    return frac[0]==0 and frac[1]!=0
+
+def cmp_frac(frac1, frac2):
+    if frac1[0]/float(frac1[1])>frac2[0]/float(frac2[1]):
+        return 1
+    elif frac1[0]/float(frac1[1])<frac2[0]/float(frac2[1]):
+        return -1
+    else:
+        return 0
+
+def frac2float(frac):
+    return frac[0]/float(frac[1])
+
+#---------TEST-------------
 
 import unittest
-
+import fracs
 class TestFractions(unittest.TestCase):
+
     def setUp(self):
-        self.f1 = Frac(1, 3)
-        self.f2 = Frac(2, 6)
-        self.f3 = Frac(3, 10)
-        self.f0 = Frac(0, 1)
+        self.zero = [0, 1]
 
     def test_add_frac(self):
-        self.assertEqual(self.f1 + self.f2 + self.f2, Frac(1))
+        self.assertEqual(fracs.add_frac([1, 2], [1, 3]), [5, 6])
 
     def test_sub_frac(self):
-        self.assertEqual(self.f2 - self.f3, Frac(1, 30))
+        self.assertEqual(fracs.sub_frac([74, 14], [18, 7]), [19, 7])
 
     def test_mul_frac(self):
-        self.assertEqual(self.f1 * self.f3, Frac(1, 10))
+        self.assertEqual(fracs.mul_frac([5,10],[1,3]),[1,6])
 
     def test_div_frac(self):
-        self.assertEqual(self.f1 / self.f3, Frac(20, 18))
-        self.assertEqual(self.f1 / self.f3, Frac(10, 9))
-        with self.assertRaises(ValueError):
-            self.f1 / 0
+        self.assertEqual(fracs.div_frac([5, 10], [1, 3]), [3, 2])
 
     def test_is_positive(self):
-        f1 = Frac(-1, -5)
-        f2 = Frac(1, -5)
-        self.assertTrue(f1.is_positive())
-        self.assertFalse(f2.is_positive())
+        self.assertEqual(fracs.is_positive([5,10]),True)
+        self.assertEqual(fracs.is_positive([-5,10]),False)
+        self.assertEqual(fracs.is_positive([-5,-10]),True)
+        self.assertEqual(fracs.is_positive([5,-10]),False)
 
     def test_is_zero(self):
-        self.assertTrue(self.f0.is_zero())
-        self.assertFalse(self.f1.is_zero())
+        self.assertEqual(fracs.is_zero([0, 1]),True)
+        self.assertEqual(fracs.is_zero([3, 2]),False)
 
     def test_cmp_frac(self):
-        self.assertTrue(self.f2 > self.f3)
-        self.assertFalse(Frac(20, 18) < Frac(10, 18))
+        self.assertEqual(fracs.cmp_frac([1,2],[2,3]),-1)
+        self.assertEqual(fracs.cmp_frac([2,3],[1,2]),1)
 
     def test_frac2float(self):
-        self.assertAlmostEqual(float(self.f1), 0.333, places=3, msg=None)
+        self.assertEqual(fracs.frac2float([1,2]),0.5)
+
+    def tearDown(self): pass
 
 if __name__ == '__main__':
     unittest.main()
