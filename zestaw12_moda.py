127b22b24bf22b212230ae6c1c9d391c7395863b
Pauline2897 <57684631+Pauline2897@users.noreply.github.com>
2020-02-17 13:07:39 +0100

    Create zestaw12_moda.py

---
+++ zestaw12_moda.py
@@ -0,0 +1,50 @@
+
+def quick_sort(L):
+    if len(L) <= 1:
+        return L
+    # pobieranie pivota wg ktorego bedzimey operowac
+    pivot = L[0]
+    less = []
+
+    # wszystkie mniejsze elementy od pivot przerzucamy do listy mniejszych
+    for x in L:
+        if x < pivot:
+            less.append(x)
+
+    # wszystkie rowne elementy przerzucamy do listy rownych
+    equal = []
+    for x in L:
+        if x == pivot:
+            equal.append(x)
+
+    # wszystkie wiesze elementy od pivot przerzucamy do listy wiekszych
+    greater = []
+    for x in L:
+        if x > pivot:
+            greater.append(x)
+
+    # (rekurencja) odnawiamy procedure dla mniejszych i wiekszych elementow
+    return quick_sort(less) + equal + quick_sort(greater)
+
+def moda(L, left, right):
+    if left >= right:
+        return None
+    L = quick_sort(L)
+
+    moda = None
+    liczebnosc1 = 0
+    i = left
+    liczebnosc2 = 1
+
+    for i in range(right - 1):
+        if L[i] == L[i+1]:
+            liczebnosc2 += 1
+            # na biezaco uaktualniamy najlepszego kandydata
+            if liczebnosc2 > liczebnosc1:  # jest lepszy kandydat
+                liczebnosc1 = liczebnosc2
+                moda = i
+        else:
+            liczebnosc2 = 1
+
+    return L[moda]
+
